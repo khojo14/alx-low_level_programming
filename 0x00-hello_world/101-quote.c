71be08b8c32ae680089588cc51e43feb552c7f39
khojo14 <n.o.lamptey12@gmail.com>
2023-02-16 18:36:44 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - a function that prints a line to the standard erroe
  *
- * Return 1 when successfull
+ * Return 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
