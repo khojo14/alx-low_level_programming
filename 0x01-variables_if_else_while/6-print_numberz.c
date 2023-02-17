a8477cd0dd75c1f808da2a2d5637c40389f9d6f7
khojo14 <n.o.lamptey12@gmail.com>
2023-02-17 14:26:21 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	char  n;
+
+	for (n = '1'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
