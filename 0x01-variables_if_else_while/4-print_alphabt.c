049cc5ebf57f4b2c9fb5c82e703a3eabf9adcd0e
khojo14 <n.o.lamptey12@gmail.com>
2023-02-17 12:14:18 -0800

    Lowercase except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,9 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z' && c != 'e' && c != 'q'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
+		if (c != 'e' && c != 'q')
 		putchar(c);
 	}
 	putchar('\n');
