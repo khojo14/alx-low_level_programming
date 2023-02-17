7e433d6438be4c602fa92b1abc5f261989419cd4
khojo14 <n.o.lamptey12@gmail.com>
2023-02-17 14:46:40 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alphabets in reverse order
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'Z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
