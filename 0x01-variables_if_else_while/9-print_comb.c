2b15fc52ee80cf53087fa190f61b10443ddeaff6
khojo14 <n.o.lamptey12@gmail.com>
2023-02-17 15:56:02 -0800

    Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alphabets in reverse order
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		while (n < 10)
+		{
+			putchar((n % 10) + '0');
+			n++;
+		}
+		putchar((n % 10) + '0');
+		n++;
+	}
+	putchar(',');
+	putchar('\t');
+
+	return (0);
+}
