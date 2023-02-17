889ce7de5f37f136072b2fa8c7d2250b0def471e
khojo14 <n.o.lamptey12@gmail.com>
2023-02-16 18:28:37 -0800

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - a function that prints a line to the standard erroe
+ *
+ * Return 1 when successfull
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
