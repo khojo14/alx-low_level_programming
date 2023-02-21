60cb729acc1601a584ad50efa23dca5eb6c506c3
khojo14 <n.o.lamptey12@gmail.com>
2023-02-21 11:37:29 -0800

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-putchar.c: prints out _putchar, followed by a new line.
