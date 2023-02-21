82a6dcfcf5dc3a136bcd0de9ee3a9a95326bf0e2
khojo14 <n.o.lamptey12@gmail.com>
2023-02-21 15:08:38 -0800

    Print lower case alphabets x10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *print_alphabet_x10 - function that prints lowercase alphabets x10
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
