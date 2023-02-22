19a459cdd125efac5dedc79b409ffb5de6e7b6a7
khojo14 <n.o.lamptey12@gmail.com>
2023-02-21 16:36:58 -0800

    9 Times Table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * time_table - prints the 9 times table, starting with 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
