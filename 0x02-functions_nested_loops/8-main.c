fe39bc6a56427570a3aac6297ba1c467f2bb7b12
khojo14 <n.o.lamptey12@gmail.com>
2023-02-21 16:26:13 -0800

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
