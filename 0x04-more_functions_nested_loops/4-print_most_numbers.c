65d84b790aac231c56968a90904c548636145064
khojo14 <n.o.lamptey12@gmail.com>
2023-02-24 18:54:42 -0800

    Quick fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
 * print_most_numbers -  prints the numbers,
-* from 0 to 9, followed by a new line.
-* @c: character to be checked
+* from 0 to 9, followed by a new line
+*
 * Return: void
 */
 
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,9 +2,8 @@
 
 /**
  * more_numbers - prints 10 times the numbers,
- * from 0 to 14, followed by a new line.
- * @i: first number
- * @j: second number
+ * from 0 to 14, followed by a new line
+ *
  * Return: no return
  */
 
