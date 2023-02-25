6815d7e98ac83cb8e1f9ed04d08f78af89715643
khojo14 <n.o.lamptey12@gmail.com>
2023-02-24 18:45:42 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@
  * but for multiples of three print Fizz instead of the number
  * and for the multiples of five print Buzz
  * for numbers which are multiples of both three and five print FizzBuzz.
- * 
+ *
  * Return: always 0
  */
 int main(void)
