81462efb81a0d329a9e99a2fbf2c459fd50f3487
khojo14 <n.o.lamptey12@gmail.com>
2023-03-02 17:14:48 -0800

    Quick fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,17 +11,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int lendest = 0;
 	int i;
 	/*char *c = dest;*/
 
-	while (dest[lendest] != '\0')
-	{
-		lendest++;
-	}
-
-	for (i = 0;  src[i] != '\0' && i < n; i++)
-		dest[i] = src[i];
+	for (i = 0; i < n && *(src + i); i++)
+		*(dest + i) = *(src + i);
+	for (; i < n; i++)
+		*(dest + i) = '\0';
 
 	return (dest);
 }
