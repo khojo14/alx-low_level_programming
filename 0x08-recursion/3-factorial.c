54726252c7894b394fea60213f1848adc56f8581
khojo14 <n.o.lamptey12@gmail.com>
2023-12-04 15:46:18 -0800

    Mandatory tasks done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number.
+ *
+ * @n: integer parameter
+ *
+ * Return:factorial of @n
+ */
+int factorial(int n)
+{
+	int fact;
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		fact = n * factorial(n - 1);
+	}
+	return (fact);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Alx - C Recursion project tasks
