1c0138c8e784750faff1a4f49300716daa96356e
khojo14 <n.o.lamptey12@gmail.com>
2024-05-29 17:33:42 -0700

    First task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
+/**
+ * main - prints the name of the program
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-	if argc > 0
+	if (argc > 0)
 		printf("%s\n", argv[0]);
+	return (0);
 }
