7d3bcbb481961dcbeeabc322b7ce13c5258e533c
khojo14 <n.o.lamptey12@gmail.com>
2024-05-29 19:36:20 -0700

    Betty fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,10 +13,11 @@ int main(int argc, char *argv[])
 	int res;
 
 	if (argc != 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
-        res = atoi(argv[1]) * atoi(argv[2]);
-        printf("%d\n", res);
-        return (0);                                                                                                                             }
+	{
+		printf("Error\n");
+		return (1);
+	}
+	res = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", res);
+	return (0);
+}
